3c97d6fcdcaa4fedd458f5c0d25af7cb6932c5a2
hu.tang <hu.tang@qingteng.cn>
2019-10-10 18:48:23 +0800

    v1.1

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+
+function getEntry() {
+  return {
+    home: ''
+  }
+}
+
+module.exports = {
+  entry: getEntry(),
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    port: '8080',
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.scss/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  }
+}
